b580ff3b56667ff93a02c5a01fed82eae27b602b
Mohamed ELJallali <jallalimohamed5@gmail.com>
2026-04-20 16:00:38 +0100

    Add secret variables and fix docker-compose file

--- server/db.py
+++ server/db.py
@@ -2,13 +2,13 @@ import mysql.connector
 import os
 import time
 
+
 def get_connection():
     db_config = {
         "host": os.getenv("MYSQL_HOST"),
         "user": os.getenv("MYSQL_USER"),
         "password": os.getenv("MYSQL_PASSWORD"),
         "database": os.getenv("MYSQL_DATABASE"),
-        "port": int(os.getenv("MYSQL_PORT"))
     }
 
     retries = 10
@@ -18,7 +18,6 @@ def get_connection():
             print("Connected to MySQL database")
             return conn
         except mysql.connector.Error as err:
-            print(f"Error connecting to MySQL: {err}")
             retries -= 1
             print(f"Retrying in 5 seconds... ({retries} retries left)")
             time.sleep(5)
